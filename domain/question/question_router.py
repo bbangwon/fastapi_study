b5d91d6e05ac3ec72152f2d3a881268dbef4299b
youngil <youngil@claion.co.kr>
2025-05-09 16:17:35 +0900

    FastAPI CRUD 추가

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
-from models import Question
+from domain.question import question_schema, question_crud
 
 router = APIRouter(
     prefix="/api/question",
 )
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
-    questions = db.query(Question).order_by(Question.create_date.desc()).all()            
+    questions = question_crud.get_question_list(db)            
     return questions
