898425f7fe20017416ab90cadbe30420aa1e6014
youngil <youngil@claion.co.kr>
2025-05-09 17:53:49 +0900

    Svelte 프로젝트 추가

---
+++ frontend/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let question_list = [];
+
+  async function get_question_list() {
+    const response = await fetch('http://127.0.0.1:8000/api/question/list')
+    if (response.ok) {
+      const data = await response.json();
+      question_list = data;
+    } else {
+      console.error('Failed to fetch question list');
+    }      
+  }
+
+  get_question_list();
+
+</script>
+
+<ul>
+  {#each question_list as question}
+    <li>{question.subject}</li>
+  {/each}
+</ul>
