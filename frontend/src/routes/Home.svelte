30325d7803cab3c8df6c2db49cce2e29866563fe
youngil <youngil@claion.co.kr>
2025-05-19 09:42:06 +0900

    페이징 처리

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,19 +1,30 @@
 <script>
   import fastapi from "../lib/api";
   import { link } from "svelte-spa-router";
+  
   let question_list = [];
+  let size = 10;
+  let page = 0;
+  let total = 0;
+  $: total_page = Math.ceil(total / size);
 
-  async function get_question_list() {
+  async function get_question_list(_page) {
+    let params = {
+      page: _page,
+      size: size
+    }
     try{
-      const data = await fastapi('get', '/api/question/list');
-      question_list = data;
+      const data = await fastapi('get', '/api/question/list', params);
+      question_list = data.question_list;
+      page = _page;
+      total = data.total;
     }
     catch (error) {
       console.error('Error fetching question list:', error);
     }
   }
 
-  get_question_list();
+  get_question_list(0);
 
 </script>
 
@@ -36,5 +47,22 @@
       {/each}
     </tbody>
   </table>
+  <!-- 페이징처리 시작 -->
+  <ul class="pagination justify-content-center">
+    <li class="page-item {page <= 0 && 'disabled'}">
+      <button class="page-link" on:click="{() => get_page_list(page - 1)}">이전</button>
+    </li>
+    {#each Array(total_page) as _, loop_page}
+    {#if loop_page >= page-5 && loop_page <= page+5}
+      <li class="page-item {loop_page === page && 'active'}">
+        <button on:click="{() => get_question_list(loop_page)}" class="page-link">{loop_page + 1}</button>
+      </li>
+    {/if}
+    {/each}
+      <li class="page-item {page >= total_page-1 && 'disabled'}">
+      <button class="page-link" on:click="{() => get_page_list(page + 1)}">다음</button>
+    </li>
+  </ul>
+  <!-- 페이징처리 끝 -->   
   <a use:link href="/question-create" class="btn btn-primary">질문하기</a>
 </div>  
